d5ee38eefcaf8cb8645a9cecc9865812f0d1ba1d
James Keane <jpkeane@me.com>
2017-05-24 16:40:23 +0100

    Added correct user tests when updating user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -48,4 +48,48 @@ RSpec.describe UsersController, type: :controller do
       end
     end
   end
+
+  context 'signed in as correct user' do
+    describe 'GET #show' do
+      it 'returns http success' do
+        right_user = FactoryGirl.create(:user)
+
+        request.session[:user_id] = right_user.id
+
+        get :show, params: { username: right_user.username }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'returns http redirect to home page' do
+        right_user = FactoryGirl.create(:user)
+
+        request.session[:user_id] = right_user.id
+
+        get :edit, params: { username: right_user.username }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'PATCH #update' do
+      it 'allows correct update of user' do
+        right_user = FactoryGirl.create(:user)
+
+        request.session[:user_id] = right_user.id
+
+        patch :update, params: {
+          username: right_user.username,
+          user: {
+            first_name: 'NewName'
+          }
+        }
+
+        expect(response).to have_http_status(:found)
+
+        right_user.reload
+        expect(right_user.first_name).to eq 'NewName'
+      end
+    end
+  end
 end
