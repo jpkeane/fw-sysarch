9aacafb058b476b53fcb21bc14750f5366bb5db7
James Keane <jpkeane@me.com>
2017-05-26 01:37:53 +0100

    Added specs for user password change with no new password

--- spec/features/user_change_password_spec.rb
+++ spec/features/user_change_password_spec.rb
@@ -25,9 +25,20 @@ RSpec.feature 'User Change Password', type: :feature do
     fill_in 'Password', with: 'NewTestPass'
     fill_in 'Password confirmation', with: 'NewTestPass'
     click_button 'Change Password'
-    expect(page).to have_content('Current password is incorrect ')
+    expect(page).to have_content('Current password is incorrect')
     user.reload
     expect(user.authenticate('NewTestPass')).not_to be_truthy
+  end
+
+  scenario 'User logs in and edits password with correct current password but no new password' do
+    successful_sign_in
+    click_link 'Edit Profile'
+    click_link 'Password'
+    fill_in 'Current password', with: user.password
+    click_button 'Change Password'
+    expect(page).to have_content('Password must be entered')
+    user.reload
+    expect(user.authenticate(user.password)).to be_truthy
   end
 
   def successful_sign_in
