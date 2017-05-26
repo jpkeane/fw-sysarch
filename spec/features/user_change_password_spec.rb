7ac8e1d743b91256409879d8b21b5600704dd16c
James Keane <jpkeane@me.com>
2017-05-26 01:28:16 +0100

    Added incorrect current password for user password change spec

--- spec/features/user_change_password_spec.rb
+++ spec/features/user_change_password_spec.rb
@@ -17,6 +17,19 @@ RSpec.feature 'User Change Password', type: :feature do
     expect(user.authenticate('NewTestPass')).to be_truthy
   end
 
+  scenario 'User logs in and edits password with wrong current password' do
+    successful_sign_in
+    click_link 'Edit Profile'
+    click_link 'Password'
+    fill_in 'Current password', with: 'NotThePassword'
+    fill_in 'Password', with: 'NewTestPass'
+    fill_in 'Password confirmation', with: 'NewTestPass'
+    click_button 'Change Password'
+    expect(page).to have_content('Current password is incorrect ')
+    user.reload
+    expect(user.authenticate('NewTestPass')).not_to be_truthy
+  end
+
   def successful_sign_in
     visit root_path
     click_link 'Log in'
