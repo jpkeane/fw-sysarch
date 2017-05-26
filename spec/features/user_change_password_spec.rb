4bb757cdc1e554586c8463f8efe7f52bedbaff73
James Keane <jpkeane@me.com>
2017-05-26 01:50:21 +0100

    Added invalid password change spec

--- spec/features/user_change_password_spec.rb
+++ spec/features/user_change_password_spec.rb
@@ -5,47 +5,54 @@ RSpec.feature 'User Change Password', type: :feature do
   let(:email) { FactoryGirl.create(:email_address, user: user) }
 
   scenario 'User logs in and edits password successfully' do
-    successful_sign_in
-    click_link 'Edit Profile'
-    click_link 'Password'
+    successful_sign_in_and_navigate
     fill_in 'Current password', with: user.password
     fill_in 'Password', with: 'NewTestPass'
     fill_in 'Password confirmation', with: 'NewTestPass'
-    click_button 'Change Password'
-    expect(page).to have_content('Password changed')
-    user.reload
+    submit_password('Password changed')
     expect(user.authenticate('NewTestPass')).to be_truthy
   end
 
   scenario 'User logs in and edits password with wrong current password' do
-    successful_sign_in
+    successful_sign_in_and_navigate
     click_link 'Edit Profile'
     click_link 'Password'
     fill_in 'Current password', with: 'NotThePassword'
     fill_in 'Password', with: 'NewTestPass'
     fill_in 'Password confirmation', with: 'NewTestPass'
-    click_button 'Change Password'
-    expect(page).to have_content('Current password is incorrect')
-    user.reload
+    submit_password('Current password is incorrect')
     expect(user.authenticate('NewTestPass')).not_to be_truthy
   end
 
   scenario 'User logs in and edits password with correct current password but no new password' do
-    successful_sign_in
-    click_link 'Edit Profile'
-    click_link 'Password'
+    successful_sign_in_and_navigate
     fill_in 'Current password', with: user.password
-    click_button 'Change Password'
-    expect(page).to have_content('Password must be entered')
-    user.reload
+    submit_password('Password must be entered')
     expect(user.authenticate(user.password)).to be_truthy
   end
 
-  def successful_sign_in
+  scenario 'User logs in and edits password with correct current password but invalid new password' do
+    successful_sign_in_and_navigate
+    fill_in 'Current password', with: user.password
+    fill_in 'Password', with: 'inv'
+    fill_in 'Password confirmation', with: 'inv'
+    submit_password('Password is too short')
+    expect(user.authenticate(user.password)).to be_truthy
+  end
+
+  def successful_sign_in_and_navigate
     visit root_path
     click_link 'Log in'
     fill_in 'Username', with: user.username
     fill_in 'Password', with: user.password
     click_button 'Log in'
+    click_link 'Edit Profile'
+    click_link 'Password'
+  end
+
+  def submit_password(expected_response)
+    click_button 'Change Password'
+    expect(page).to have_content(expected_response)
+    user.reload
   end
 end
